f537acaea2d413dab74c672d69239928cd488022
Lê Hải Dương <duonglh.124010121040@vtc.edu.vn>
2021-11-12 22:20:19 +0700

    Bai Tap C# _ Tuan 1

---
+++ BT2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BT2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Ten;
+            Ten = Console.ReadLine();
+            Console.WriteLine("Hello " + Ten);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BT3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BT3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C = 0;
+            Console.Write("Nhap So thu nhat: ");
+            A = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap So thu hai: ");
+            B = Convert.ToInt32(Console.ReadLine());
+            C = A + B;
+            Console.WriteLine("Tong cua " + A + " Va " + B + " La " + C);
+            Console.ReadLine();
+        }
+    }
+}
