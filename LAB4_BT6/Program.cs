49d44f7ac81a1cb3db581ba95fdbb06fabea3009
lehaiduong769 <93021340+lehaiduong769@users.noreply.github.com>
2021-11-23 08:47:46 +0700

    BT LAB4 _C#_ Tuan 03

---
+++ LAB4_BT6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LAB4_BT6
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int n1 = 1, n2 = 1, n3, i, n;
+            Console.Write("Nhap vao so n: ");
+            n = int.Parse(Console.ReadLine());
+            Console.Write(n1 + " " + n2 + " ");  
+            for (i = 2; i < n; i++)    
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
