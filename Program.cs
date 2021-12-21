d3767ca622bdac5fef449d70eee8baf7f6082cb3
lehaiduong769 <93021340+lehaiduong769@users.noreply.github.com>
2021-12-21 09:06:04 +0700

    LAB 8 _ BT4 _Xulychuoi

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BT4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap vao mot chuoi ky tu");
+            string str = Console.ReadLine();
+            string strResult = "";
+            
+            str = str.Trim();
+            while (str.IndexOf("  ") != -1)
+            {
+                str = str.Replace("  ", " ");
+            }
+            string[] substr = str.Split(' ');
+           
+            for (int i = 0; i < substr.Length; i++)
+            {
+                if ((str[i] >= '0' && str[i] <= '9') || (str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z') || (str[i] == ' '))
+                {
+                    string firstchar = substr[i].Substring(0, 1);
+                    string otherchar = substr[i].Substring(1);
+                    substr[i] = firstchar.ToUpper() + otherchar.ToLower();
+                    strResult += substr[i] + " ";
+                   
+                }
+            }
+            Console.WriteLine($"Chuoi sau khi duoc chuan hoa: {strResult} ");
+            Console.ReadKey();
+        }
+    }
+}
