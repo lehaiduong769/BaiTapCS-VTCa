1c62ea9c2626b2f72dce6a155a6b51fa004fe4b4
lehaiduong769 <93021340+lehaiduong769@users.noreply.github.com>
2021-12-25 08:38:37 +0700

    Lab9_BT2_Stack

--- Program.cs
+++ Program.cs
@@ -1,141 +1,69 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 
-namespace BT1
+namespace BT2_Stack
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int choice, sophantu;
-            var List = new List<int>();
-            Console.WriteLine(" Nhap so phan tu co trong ds: ");
-            sophantu = int.Parse(Console.ReadLine());
-            for (int i = 0; i < sophantu; i++)
+            int choice, n;
+            Stack stack = new Stack();
+            Console.WriteLine(" Nhap so phan tu muon khoi tao trong Stack: ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
             {
-                List.Add(new Random().Next(0, 10));
+                stack.Push(new Random().Next(0, 10));
             }
-        
             do
             {
-                Console.WriteLine("Demo cac thao tac tren ArrayList so nguyen trong C#");
-                Console.WriteLine("*==================================================");
-                Console.WriteLine("1.   Khoi tao danh sach");
-                Console.WriteLine("2.   Them mot phan tu");
-                Console.WriteLine("3.   Xoa mot phan tu");
-                Console.WriteLine("4.   Chen mot phan tu");
-                Console.WriteLine("5.   Duyet danh sach");
-                Console.WriteLine("6.   Tim mot phan tu");
-                Console.WriteLine("7.   Sap xep danh sach tang dan");
-                Console.WriteLine("8.   Sap xep danh sach giam dan");
+                Console.WriteLine("Demo cac thao tac tren Stack so nguyen C#");
+                Console.WriteLine("=========================================");
+                Console.WriteLine("1.   Khoi tao Stack");
+                Console.WriteLine("2.   Them mot phan tu vao Stack");
+                Console.WriteLine("3.   Lay phan tu dau ra khoi Stack");
+                Console.WriteLine("4.   Xem phan tu dau cua Stack");
+                Console.WriteLine("5.   Duyet Stack");
                 Console.WriteLine("0.   Thoat chuong trinh");
                 Console.Write("    NHAP LUA  CHON CUA BAN VAO:");
                 choice = int.Parse(Console.ReadLine());
-
                 switch (choice)
                 {
                     case 1:
-                        Console.WriteLine($"Original List: ");
-                        foreach (int num in List)
-                        {
-                            Console.Write(num + " ");
-                        }
-                        Console.WriteLine();
-
-                        break;
-
-                    case 2:
-                        Console.Write("Nhap vao phan tu muon them vao: ");
-                        int nadd = int.Parse(Console.ReadLine());
-                        List.Add(nadd);
-                        Console.WriteLine($"List after adding {nadd} : ");
-                        foreach (int num in List)
-                        {
-                            Console.Write(num + " ");
-                        }
-                        Console.WriteLine();
-                        break;
-
-                    case 3:
-                        Console.Write("Nhap vao gia tri phan tu muon remove:");
-                        int nremove = int.Parse(Console.ReadLine());
-                        List.Remove(nremove);
-                        Console.WriteLine($"List after removing {nremove}: ");
-                        foreach (int num in List)
-                        {
-                            Console.Write(num + " ");
-                        }
-                        Console.WriteLine();
-                        break;
-
-                    case 4:
-                        Console.Write("Nhap vao gia tri phan tu can chen:");
-                        int value = int.Parse(Console.ReadLine());
-                        Console.Write("Nhap vao vi tri phan tu can chen:");
-                        int index = int.Parse(Console.ReadLine());
-                        List.Insert(index, value);
-                        Console.WriteLine($"List after inserting: ");
-                        foreach (int num in List)
-                        {
-                            Console.Write(num + " ");
-                        }
-                        Console.WriteLine();
-                        break;
-
-                    case 5:
-                        
-                        Console.WriteLine($"Danh sach sau khi duyet: {List.GetRange(0, List.Count)}");
-                        break;
-
-                    case 6:
-                        Console.Write("Nhap vao gia tri phan tu can tim: ");
-                        int nsearch = int.Parse(Console.ReadLine());
-                        bool isExists = List.Contains(nsearch);
-                        if (isExists == false)
-                        {
-                            Console.WriteLine(" Khong tim thay phan tu nay trong List");
-                        }
-                        else
-                        {
-                            Console.Write($"Phan tu nam o vi tri: ");
-                            for (int i = 0; i < sophantu; i++)
-                            {
-                                if (List[i] == nsearch)
-                                {
-                                    Console.Write(i + " ");
-                                }
-                            }
-                            Console.WriteLine();
-                        }
-                        break;
-                    case 7:
-                        List.Sort();
-                        Console.WriteLine("Danh sach duoc xep theo thu tu tang dan: ");
-                        foreach (int num in List)
-                        {
-                            Console.Write(num + " ");
-                        }
-                        Console.WriteLine();
-                        break;
-                    case 8:
-                        List.Reverse();
-                        Console.WriteLine("Danh sach duoc xep theo thu tu giam dan: ");
-                        foreach (int num in List)
+                        Console.WriteLine("Ds cac phan tu duoc khoi tao trong STack: ");
+                        foreach (int num in stack)
                         {
                             Console.Write(num + " ");
                         }
                         Console.WriteLine(); break;
 
-                    case 0:
-                        break;
+                    case 2:
+                        Console.WriteLine("Nhap vao gia tri phan tu muon them vao Stack: ");
+                        int m = int.Parse(Console.ReadLine());
+                        stack.Push(m);
+                        foreach(int num in stack)
+                        {
+                            Console.Write(num + " "); 
+                        }
+                        Console.WriteLine(); break;
+                    case 3:
+                        Console.WriteLine("Phan tu dau duoc lay ra khoi Stack la: ");
+                        Console.WriteLine(" " + stack.Pop());
+                        Console.WriteLine(); break;
+                    case 4:
+                        Console.WriteLine($"Xem phan tu dau cua Stack la: {stack.Peek()}");
+                        Console.WriteLine(); break;
+                    case 5:
+                        Console.Write("So phan tu con lai trong Stack la: {0}",stack.Count);
+                        Console.WriteLine(); break;
+                    case 0: break;
                     default:
-                        Console.WriteLine("vui long nhap lai");
-                        break;
-                }
+                        Console.WriteLine(" Vui long nhap lai!");break;
 
-            } while (choice != 0);
-            Console.ReadLine();
+                }
             }
+            while (choice != 0); Console.ReadKey();
+
+        }
     }
 }
